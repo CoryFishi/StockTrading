300dbb6ee44b76aacff2e951a8d2729435919153
Cory Fishburn <coryjr2002@gmail.com>
2025-04-24 00:40:00 -0700

    Fix: Added setStocks to the creation modal so it can append the new stock

--- stock-trading-system/src/pages/Admin.jsx
+++ stock-trading-system/src/pages/Admin.jsx
@@ -1,12 +1,12 @@
 import Navbar from "../components/Navbar";
 import StocksPage from "../components/AdminPages/StocksPage";
 
-export default function Admin({ stocks, darkMode, toggleDarkMode }) {
+export default function Admin({ stocks, setStocks, darkMode, toggleDarkMode }) {
   return (
     <div className="h-screen flex flex-col">
       <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
       <div className="flex-1 overflow-auto">
-        <StocksPage s={stocks} />
+        <StocksPage stocks={stocks} setStocks={setStocks} />
       </div>
     </div>
   );
